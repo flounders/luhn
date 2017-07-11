d7da8283f69891b2f90ae56246d5efa6df8237de
Steven Williams <steven.m.williams@yandex.com>
2017-07-10 20:03:40 -0500

    Add additional tests for invalid and American Express numbers

--- spec/luhn/credit_check_spec.rb
+++ spec/luhn/credit_check_spec.rb
@@ -9,6 +9,20 @@ module Luhn
 
         expect(credit_check.valid?).to eq(true)
       end
+
+      it 'returns false on an invalid number' do
+        credit_check = CreditCheck.new("5541801923795240")
+
+        expect(credit_check.valid?).to eq(false)
+      end
+
+      it 'returns expected values for American Express card numbers' do
+        valid = CreditCheck.new("342804633855673")
+        invalid = CreditCheck.new("342801633855673")
+
+        expect(valid.valid?).to eq(true)
+        expect(invalid.valid?).to eq(false)
+      end
     end
   end
 end
